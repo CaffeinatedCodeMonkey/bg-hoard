cd193c24ebcf3001545f36b51864faa42b0ae365
travis.jones <travis.jones@cdinteractive.com>
2021-09-14 16:30:32 -0400

    Lab 11.

--- apps/store-ui-shared-e2e/src/integration/header.component.spec.ts
+++ apps/store-ui-shared-e2e/src/integration/header.component.spec.ts
@@ -3,4 +3,11 @@ describe('store-ui-shared', () => {
   it('should render the component', () => {
     cy.get('bg-hoard-header').should('exist');
   });
+
+  it('should show the title', () => {
+    cy.visit(
+      '/iframe.html?id=headercomponent--primary&args=title:BoardGameHoard;'
+    );
+    cy.get('bg-hoard-header').contains('BoardGameHoard');
+  });
 });
